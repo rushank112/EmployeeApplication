6acdffb4e138b9677f5d2c8202357f5ee1688bd7
Engu Rushank <engu.rushank@globallogic.com>
2024-06-27 11:24:34 +0530

    first commit

---
+++ src/main/java/com/gl/app/EmployeeApplication/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.gl.app.EmployeeApplication.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employee")
+    public ResponseEntity<String> getDetails(){
+        return new ResponseEntity<>("Employee Details", HttpStatus.OK);
+    }
+}
